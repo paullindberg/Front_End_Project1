7d77c366cd49c7197f2b5eb966104770ff0df50d
paullindberg <47434907+paullindberg@users.noreply.github.com>
2020-03-22 13:16:37 -0700

    now using new homepage

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -1,5 +1,14 @@
+document.getElementById("landing").innerHTML = "Logged in as " + sessionStorage.sessionName;
+
 function Logout(){
     sessionStorage.sessionName = "";
     window.location.href = "index.html"
 
+}
+
+function CreateTopic(){
+
+
+
+    
 }
