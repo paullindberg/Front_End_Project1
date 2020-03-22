0a52a5716008674030cd4f1c08a7ee31a780276c
mesorangerxx <lqtruongnguyen@gmail.com>
2020-03-22 16:46:43 -0700

    push

---
+++ gallery.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8"/>
+  <title>Gallery</title>
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css">
+
+</head>
+  <body>
+    <a href="upload.html"><-upload more photos</a>
+    <h1>Photo gallery</h1>
+    <div align="center">
+      
+      
+
+
+
+<?php
+  //initialize db connection
+  $db = mysqli_connect("localhost", "root", "", "image_upload");
+
+
+//check fann_get_total_connections
+if (!$db) {
+  die("Connection to db failed: " . mysqli_connect_error());
+}
+
+  // Initialize message variable
+  $msg = "";
+
+  //When upload button is clicked
+  if (isset($_POST['upload'])) {
+  	//image file name
+  	$image = $_FILES['image']['name'];
+  	//gets user inputted name/date/photog/loc
+    $name = mysqli_real_escape_string($db, $_POST['name']);
+
+  	//target image file directory
+  	$target = "uploads/".basename($image);
+
+    //insert values into db
+    //image BLOB?
+
+
+    //////////////////////////////////////////
+    // sql to create table
+    $sql = "CREATE TABLE IF NOT EXISTS images (
+    id INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    image varchar(100) NOT NULL,
+    name TEXT NOT NULL
+    )";
+
+    if ($db->query($sql) === TRUE) {
+      
+    } else {
+        echo "Error creating table: " . $db->error;
+      }
+    //////////////////////////////////////////
+
+
+
+  	$sql = "INSERT INTO images (image, name) VALUES ('$image','$name')";
+
+
+  	mysqli_query($db, $sql);
+  	if (move_uploaded_file($_FILES["image"]["tmp_name"],"uploads/".$_FILES["image"]["name"])) {
+      //if file uploaded, successful msg
+  		$msg = "Successful";
+  	}else{
+  		$msg = "Failed to upload";
+  	}
+  }
+
+  $result = mysqli_query($db, "SELECT * FROM images");
+  //query, get all from db
+  //print image and data
+  while ($row = mysqli_fetch_array($result)) {
+    echo "<div id='img_div'>";
+      echo "<img src='uploads/".$row['image']."' height=15% width=15% >";
+      echo "<p>".$row['name']."</p>";
+    echo "</div>";
+}
+
+?>
+
+
+      </div>
+    </div>
+  </div>
+</body>
+</html>
---
+++ userinfo.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8"/>
+  <title>Sign up results</title>
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css">
+
+</head>
+  <body>
+    
+    <h1>Sign up results</h1>
+    <div align="center">
+
+
+
+<?php
+  //initialize db connection
+  $db = mysqli_connect("localhost", "pma", "", "image_upload");
+
+
+//check fann_get_total_connections
+if (!$db) {
+  die("Connection to db failed: " . mysqli_connect_error());
+}
+
+  // Initialize message variable
+  $msg = "";
+
+  //When sign up button is clicked
+  if (isset($_POST['signup'])) {
+  	
+  	//gets user inputted name/date/photog/loc
+    $username = mysqli_real_escape_string($db, $_POST['username']);
+    $password = mysqli_real_escape_string($db, $_POST['password']);
+    $name = mysqli_real_escape_string($db, $_POST['name']);
+    $email = mysqli_real_escape_string($db, $_POST['email']);
+
+
+
+
+    //////////////////////////////////////////
+    // sql to create table
+    $sql = "CREATE TABLE IF NOT EXISTS user_info (
+    username VARCHAR(11) NOT NULL PRIMARY KEY,
+    password TEXT NOT NULL,
+    name TEXT NOT NULL,
+    email TEXT NOT NULL
+    )";
+
+    if ($db->query($sql) === TRUE) {
+      
+    } else {
+        echo "Error creating table: " . $db->error;
+      }
+    //////////////////////////////////////////
+
+    $query = mysql_query("SELECT * FROM user_info WHERE username='$username'");
+    if(mysql_num_rows($query) > 0){
+      echo 'Username already exists';
+    }else{
+      $sql = "INSERT INTO users (username, password, name, email) VALUES ('$username','$password', '$name', 'email')";
+      header("location:index.html");
+      mysqli_query($db, $sql);
+    }
+
+    //insert values into db
+    
+  	//$sql = "INSERT INTO user_info (username, password, name, email) VALUES ('$username','$password', '$name', 'email')";
+
+
+  	//mysqli_query($db, $sql);
+  	
+  }
+
+  /*$result = mysqli_query($db, "SELECT * FROM user_info");
+  //query, get all from db
+  //print image and data
+  while ($row = mysqli_fetch_array($result)) {
+    echo "<div id='img_div'>";
+      echo "<p>".$row['username']."</p>";
+      echo "<p>".$row['password']."</p>";
+      echo "<p>".$row['name']."</p>";
+      echo "<p>".$row['email']."</p>";
+    echo "</div>";
+}*/
+
+?>
+
+
+      </div>
+    </div>
+  </div>
+</body>
+</html>
